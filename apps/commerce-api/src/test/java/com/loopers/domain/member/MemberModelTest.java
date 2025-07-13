a18d29b07ac32ba4c416315f356dbaee092a1ada
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2025-07-13 23:14:06 +0900

    feat: 회원 모델 추가 및 테스트 작성 (#1)

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/member/MemberModelTest.java
@@ -0,0 +1,93 @@
+package com.loopers.domain.member;
+
+import com.loopers.domain.member.enums.Gender;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberModelTest {
+
+    @DisplayName("회원 모델을 생성할 때, ")
+    @Nested
+    class Create {
+        @DisplayName("아이디와 성별과 생일과 이메일이 모두 주어지면, 정상적으로 생성된다.")
+        @Test
+        void createMemberModel_whenUserIdAndGenderAndBirthdateAndEmailAreProvided() {
+            String userId = "test";
+            Gender gender = Gender.MALE;
+            String birthdate = "2020-01-01";
+            String email = "test@test.com";
+
+            MemberModel memberModel = new MemberModel(userId, gender, birthdate, email);
+
+            assertAll(
+                    () -> assertThat(memberModel.getId()).isNotNull(),
+                    () -> assertThat(memberModel.getUserId()).isEqualTo(userId),
+                    () -> assertThat(memberModel.getGender()).isEqualTo(gender),
+                    () -> assertThat(memberModel.getBirthdate()).isEqualTo(
+                            LocalDate.of(2020, 1, 1)
+                    ),
+                    () -> assertThat(memberModel.getEmail()).isEqualTo(email),
+                    () -> assertThat(memberModel.getPoints()).isEqualTo(0L)
+            );
+        }
+
+        @DisplayName("아이디가 빈칸 혹은 null 이면, BAD_REQUEST 예외가 발생한다")
+        @Test
+        void throwsBadRequestException_whenUserIdIsNullOrBlank() {
+            String blankUserId = "     ";
+
+            CoreException resultWhenNull = assertThrows(CoreException.class, () -> {
+                new MemberModel(null, Gender.MALE, "2020-01-01", "test@test.com");
+            });
+            assertThat(resultWhenNull.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+
+            CoreException resultWhenBlank = assertThrows(CoreException.class, () -> {
+                new MemberModel(blankUserId, Gender.MALE, "2020-01-01", "test@test.com");
+            });
+            assertThat(resultWhenBlank.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("아이디가 영문 및 숫자 10자 이내 형식에 맞지 않으면, Member 객체 생성에 실패한다.")
+        @Test
+        void throwsBadRequestException_whenUserIdIsNotAlphanumericOrExceedsTenCharacters() {
+            String lengthExceedsTen = "abcde12345678910";
+            String isNotAlphanumeric = "!@#한글";
+
+            CoreException resultWhenLengthExceedsTen = assertThrows(CoreException.class, () -> {
+                new MemberModel(lengthExceedsTen, Gender.MALE, "2020-01-01", "test@test.com");
+            });
+            assertThat(resultWhenLengthExceedsTen.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+
+            CoreException resultWhenIsNotAlphanumeric = assertThrows(CoreException.class, () -> {
+                new MemberModel(isNotAlphanumeric, Gender.MALE, "2020-01-01", "test@test.com");
+            });
+            assertThat(resultWhenIsNotAlphanumeric.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("이메일이 xx@yy.zz 형식에 맞지 않으면, Member 객체 생성에 실패한다.")
+        @Test
+        void throwsBadRequestException_whenEmailIsNotValid() {
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new MemberModel("test", Gender.MALE, "2020-01-01", "@not.email.com");
+            });
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+
+        @DisplayName("생년월일이 yyyy-MM-dd 형식에 맞지 않으면, Member 객체 생성에 실패한다.")
+        @Test
+        void throwsBadRequestException_whenBirthdateIsNotValid() {
+            CoreException result = assertThrows(CoreException.class, () -> {
+                new MemberModel("test", Gender.MALE, "20/01/01T05:30", "test@test.com");
+            });
+            assertThat(result.getErrorType()).isEqualTo(ErrorType.BAD_REQUEST);
+        }
+    }
+}
