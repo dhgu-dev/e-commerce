da3b7200ebf92e6c39a84c25655f7018a4bc850c
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2025-07-16 08:27:48 +0900

    feat: 내 정보 조회 기능 구현 및 테스트 작성 (#3)
    * feat: 내 정보 조회 API 구현 및 테스트 작성
    
    * feat: 내 정보 조회 통합 테스트 작성

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/member/MemberV1ApiSpec.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/member/MemberV1ApiSpec.java
@@ -4,6 +4,7 @@ import com.loopers.interfaces.api.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "Member V1 API", description = "회원 API 입니다")
 public interface MemberV1ApiSpec {
@@ -14,5 +15,13 @@ public interface MemberV1ApiSpec {
     )
     ApiResponse<MemberV1Dto.MemberResponse> signup(
             @RequestBody MemberV1Dto.SignupRequest dto
+    );
+
+    @Operation(
+            summary = "내 정보 조회",
+            description = "로그인한 사용자의 정보를 조회합니다."
+    )
+    ApiResponse<MemberV1Dto.MemberResponse> getMyInfo(
+            @RequestHeader("X-USER-ID") String userId
     );
 }
--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/member/MemberV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/member/MemberV1Controller.java
@@ -3,10 +3,15 @@ package com.loopers.interfaces.api.member;
 import com.loopers.application.member.MemberFacade;
 import com.loopers.application.member.MemberInfo;
 import com.loopers.interfaces.api.ApiResponse;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +29,16 @@ public class MemberV1Controller implements MemberV1ApiSpec {
                 dto.birthdate(),
                 dto.email()
         );
+        return ApiResponse.success(MemberV1Dto.MemberResponse.from(member));
+    }
+
+    @GetMapping("/me")
+    @Override
+    public ApiResponse<MemberV1Dto.MemberResponse> getMyInfo(String userId) {
+        MemberInfo member = memberFacade.getMember(userId);
+        if (member == null) {
+            throw new CoreException(ErrorType.NOT_FOUND, MessageFormat.format("[userId = {0}] 회원을 찾을 수 없습니다.", userId));
+        }
         return ApiResponse.success(MemberV1Dto.MemberResponse.from(member));
     }
 }
