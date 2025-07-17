d6f9ba813c1e5b82f7085791039b96da66b84b0d
dhgu-dev <82129110+dhgu-dev@users.noreply.github.com>
2025-07-18 01:40:50 +0900

    feat: 보유 포인트 조회 기능 구현 및 테스트 작성 (#5)

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/points/PointsV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/points/PointsV1Controller.java
@@ -2,7 +2,10 @@ package com.loopers.interfaces.api.points;
 
 import com.loopers.application.points.PointsFacade;
 import com.loopers.interfaces.api.ApiResponse;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +22,14 @@ public class PointsV1Controller implements PointsV1ApiSpec {
     public ApiResponse<PointsV1Dto.PointsResponse> charge(String userId, PointsV1Dto.PointsChargeRequest dto) {
         Long currentPoints = pointsFacade.chargePoints(userId, dto.amount());
         return ApiResponse.success(new PointsV1Dto.PointsResponse(currentPoints));
+    }
+
+    @GetMapping()
+    @Override
+    public ApiResponse<PointsV1Dto.PointsResponse> getMyPoints(String userId) {
+        if (userId == null || userId.isBlank()) {
+            throw new CoreException(ErrorType.BAD_REQUEST);
+        }
+        return ApiResponse.success(new PointsV1Dto.PointsResponse(pointsFacade.getPoints(userId)));
     }
 }
--- apps/commerce-api/src/test/java/com/loopers/application/member/PointsFacadeIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/application/member/PointsFacadeIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.loopers.application.member;
 
 import com.loopers.application.points.PointsFacade;
+import com.loopers.domain.member.MemberModel;
 import com.loopers.domain.member.MemberService;
+import com.loopers.domain.member.enums.Gender;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import com.loopers.utils.DatabaseCleanUp;
@@ -15,6 +17,9 @@ import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class PointsFacadeIntegrationTest {
@@ -46,6 +51,35 @@ public class PointsFacadeIntegrationTest {
             CoreException exception = assertThrows(CoreException.class, () -> pointsFacade.chargePoints(notExistUserId, points));
 
             assertThat(exception.getErrorType()).isEqualTo(ErrorType.NOT_FOUND);
+        }
+    }
+
+    @DisplayName("보유 포인트를 조회할 때")
+    @Nested
+    class GetPoints {
+
+        @DisplayName("해당 ID 의 회원이 존재할 경우, 보유 포인트가 반환된다.")
+        @Test
+        void returnsPoints_whenExistUserId() {
+            String userId = "testUser";
+            Long initialPoints = 1000L;
+            doReturn(new MemberModel(userId, Gender.FEMALE, "2000-01-01", "test@test.com", initialPoints)).when(memberService).getMember(userId);
+
+            Long points = pointsFacade.getPoints(userId);
+
+            assertThat(points).isEqualTo(initialPoints);
+        }
+
+        @DisplayName("해당 ID 의 회원이 존재하지 않을 경우, null 이 반환된다.")
+        @Test
+        void returnsNull_whenNotExistUserId() {
+            String notExistUserId = "notTestUser";
+            doThrow(new CoreException(ErrorType.NOT_FOUND))
+                    .when(memberService).getMember(eq(notExistUserId));
+
+            Long points = pointsFacade.getPoints(notExistUserId);
+
+            assertThat(points).isNull();
         }
     }
 }
