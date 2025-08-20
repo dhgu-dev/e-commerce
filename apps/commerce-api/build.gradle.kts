860f0c19a15571ee3b48fb94aa04c4776297b11e
구동현 <dhgu.dev@gmail.com>
2025-08-21 01:32:53 +0900

    feat: Feign 및 Resilience4j 설치와 세팅

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -11,6 +11,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
 
+    // Feign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    // Resilience4j - Feign 통합을 위한 라이브러리
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    implementation("io.github.resilience4j:resilience4j-feign:2.2.0")
+
     // querydsl
     annotationProcessor("com.querydsl:querydsl-apt::jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api")
--- apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
+++ apps/commerce-api/src/main/java/com/loopers/CommerceApiApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import java.util.TimeZone;
@@ -13,6 +14,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableFeignClients
 public class CommerceApiApplication {
 
     public static void main(String[] args) {
